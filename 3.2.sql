4c8ba3ca6f0764e065abde6e09736947b39c910c
nicobeltran7 <nico.beltranrubio@gmail.com>
2025-11-03 14:21:29 -0500

    Delete 3.2.sql

--- 3.2.sql
+++
@@ -1,9 +0,0 @@
-SELECT athlete_full_name, athlete_year_birth
-FROM athletes
-WHERE first_game = 'Beijing 2022'
-ORDER BY athlete_year_birth DESC
-
-SELECT athlete_full_name, athlete_year_birth
-FROM athletes
-WHERE first_game = 'Beijing 2022'
-ORDER BY athlete_year_birth ASC
